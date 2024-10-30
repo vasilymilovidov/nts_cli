730ba39336b6e3f7dd8d5ed489da13f56cd6679a
vasiliimilovidov <vasily.milovidov@gamil.com>
2024-10-30 19:09:52 +0100

    refactor UI

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod mp3_decoder;
 use crossterm::{
     event::{self, Event, KeyCode, KeyEvent},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{disable_raw_mode, LeaveAlternateScreen},
 };
 use mp3_decoder::Mp3StreamDecoder;
 use ratatui::{
@@ -18,7 +18,10 @@ use ratatui::{
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style, Stylize},
     text::{Line, Span},
-    widgets::{Block, Borders, List, ListItem, Paragraph, Wrap},
+    widgets::{
+        Block, Borders, List, ListItem, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState,
+        Wrap,
+    },
     Terminal,
 };
 use reqwest::blocking::Client;
@@ -46,6 +49,9 @@ const STREAM_URL_1: &str = "https://stream-mixtape-geo.ntslive.net/stream";
 const STREAM_URL_2: &str = "https://stream-mixtape-geo.ntslive.net/stream2";
 const DEFAULT_DURATION_SEC: u64 = 5;
 const DEFAULT_VOLUME: f32 = 1.0;
+const RECOGNITION_INFO_TIMER: u64 = 12;
+const DURATION_INFO_TIMER: u64 = 1;
+const VOLUME_INFO_TIMER: u64 = 2;
 
 //
 // MAIN
@@ -55,22 +61,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (ui_tx, ui_rx): (Sender<UIMessage>, Receiver<UIMessage>) = mpsc::channel();
     let ui_tx_clone = ui_tx.clone();
 
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut terminal = ratatui::init();
     let mut radio = Radio::new(ui_tx_clone);
 
     ui_tx.send(UIMessage::UpdateUI).unwrap();
 
     let ui_tx_clone = ui_tx.clone();
     thread::spawn(move || loop {
-        if let Event::Key(key) = event::read().unwrap() {
-            ui_tx.send(UIMessage::KeyPress(key)).unwrap();
-        } else if let Event::Resize(_, _) = event::read().unwrap() {
-            ui_tx.send(UIMessage::UpdateUI).unwrap();
-        }
+        match event::read().unwrap() {
+             Event::Key(key) => ui_tx.send(UIMessage::KeyPress(key)).unwrap(),
+             Event::Resize(_, _) => ui_tx.send(UIMessage::UpdateUI).unwrap(),
+             _ => {}
+         }
     });
 
     thread::spawn(move || loop {
@@ -203,10 +205,25 @@ struct Radio {
     _stream: Option<OutputStream>,
     volume: f32,
     volume_display_timeout: Option<SystemTime>,
+    duration_display_timeout: Option<SystemTime>,
+    recognition_result_display_timeout: Option<SystemTime>,
+    recognition_list: String,
+    vertical_scroll_state: ScrollbarState,
+    vertical_scroll: usize,
 }
 
 impl Radio {
     fn new(ui_tx: Sender<UIMessage>) -> Self {
+        let mut buf = String::new();
+        let history_file_path = get_history_file_path();
+        let _ = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .read(true)
+            .open(history_file_path)
+            .unwrap()
+            .read_to_string(&mut buf);
+        let history_len = buf.lines().count();
         let streams_collection = StreamsCollection::populate_collection().unwrap();
         let selected_stream_index = 0;
         let (recognition_result_tx, recognition_result_rx) = mpsc::channel();
@@ -224,6 +241,11 @@ impl Radio {
             _stream: None,
             volume: DEFAULT_VOLUME,
             volume_display_timeout: None,
+            duration_display_timeout: None,
+            recognition_result_display_timeout: None,
+            recognition_list: buf,
+            vertical_scroll_state: ScrollbarState::default(),
+            vertical_scroll: history_len,
         }
     }
 
@@ -325,9 +347,29 @@ impl Radio {
         });
     }
 
+    fn start_recognition_info_timer(&self) {
+        let ui_tx = self.ui_tx.clone();
+        thread::spawn(move || {
+            thread::sleep(Duration::from_secs(RECOGNITION_INFO_TIMER));
+            let _ = ui_tx.send(UIMessage::UpdateUI);
+        });
+    }
+    
     fn handle_recognition_result(&mut self) {
         if let Ok(result) = self.recognition_result_rx.try_recv() {
             self.recognition_result = Some(result);
+            let mut buf = String::new();
+            let history_file_path = get_history_file_path();
+            let _ = OpenOptions::new()
+                .create(true)
+                .append(true)
+                .read(true)
+                .open(history_file_path)
+                .unwrap()
+                .read_to_string(&mut buf);
+            self.recognition_list = buf;
+            self.recognition_result_display_timeout = Some(SystemTime::now());
+            self.start_recognition_info_timer();
         }
     }
 
@@ -336,7 +378,7 @@ impl Radio {
         terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
     ) -> Result<(), Box<dyn std::error::Error>> {
         terminal.draw(|f| {
-            let chunks = Layout::default()
+            let main_chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(1)
                 .constraints(
@@ -348,17 +390,17 @@ impl Radio {
                     .as_ref(),
                 )
                 .split(f.area());
-
+    
             let top_chunks = Layout::default()
                 .direction(Direction::Horizontal)
                 .constraints([Constraint::Percentage(25), Constraint::Percentage(50)].as_ref())
-                .split(chunks[1]);
-
+                .split(main_chunks[1]);
+    
             let bottom_chunks = Layout::default()
                 .direction(Direction::Vertical)
-                .constraints([Constraint::Fill(1), Constraint::Fill(1)].as_ref())
-                .split(chunks[2]);
-
+                .constraints([Constraint::Fill(1), Constraint::Fill(1), Constraint::Fill(1)].as_ref())
+                .split(main_chunks[2]);
+    
             let create_list_item = |title: &str, is_selected: bool| {
                 let style = if is_selected {
                     Style::default().fg(Color::Red).add_modifier(Modifier::BOLD)
@@ -367,21 +409,23 @@ impl Radio {
                 };
                 ListItem::new(vec![Line::from(Span::styled(title.to_string(), style))])
             };
-
+    
+            // Create list items for mixtapes and stations
             let stream_items_mixtapes: Vec<ListItem> = self.streams_collection
                 .mixtapes
                 .iter()
                 .enumerate()
                 .map(|(i, mixtape)| create_list_item(&mixtape.title, i + 2 == self.selected_stream_index))
                 .collect();
-
+    
             let stream_items_stations: Vec<ListItem> = self.streams_collection
                 .stations
                 .iter()
                 .enumerate()
                 .map(|(i, station)| create_list_item(&station.title, i == self.selected_stream_index))
                 .collect();
-
+    
+            // Render live stations list
             let live_stations_list = List::new(stream_items_stations)
                 .block(create_block("Stations"))
                 .highlight_style(
@@ -389,9 +433,10 @@ impl Radio {
                         .fg(Color::Yellow)
                         .add_modifier(Modifier::BOLD),
                 );
-
-            f.render_widget(live_stations_list, chunks[0]);
-
+    
+            f.render_widget(live_stations_list, main_chunks[0]);
+    
+            // Render mixtape list
             let mixtape_list = List::new(stream_items_mixtapes)
                 .block(create_block("Mixtapes"))
                 .highlight_style(
@@ -399,9 +444,9 @@ impl Radio {
                         .fg(Color::Yellow)
                         .add_modifier(Modifier::BOLD),
                 );
-
+    
             f.render_widget(mixtape_list, top_chunks[0]);
-
+    
             let (description, subtitle) = if self.selected_stream_index < 2 {
                 let station = &self.streams_collection.stations[self.selected_stream_index];
                 (station.description.clone(), station.subtitle.clone())
@@ -410,8 +455,9 @@ impl Radio {
                 let mixtape = &self.streams_collection.mixtapes[mixtape_index];
                 (mixtape.description.clone(), mixtape.subtitle.clone())
             };
-
-            let description = Paragraph::new(vec![
+    
+            // Render description
+            let description_paragraph = Paragraph::new(vec![
                 Line::from(vec![
                     Span::styled(subtitle, Style::new().green().italic()),
                 ]),
@@ -420,30 +466,63 @@ impl Radio {
             ])
             .block(create_block("Description"))
             .wrap(Wrap { trim: true });
-
-            f.render_widget(description, top_chunks[1]);
-
-            let mut recognition_text = self.recognition_result
+    
+            f.render_widget(description_paragraph, top_chunks[1]);
+    
+            // Render recognition result and list
+            let recognition_result_text = self.recognition_result
                 .clone()
                 .unwrap_or_else(|| "Recognizing...".to_string());
+            let recognition_list = self.recognition_list.clone().to_string();
+            self.vertical_scroll_state = self.vertical_scroll_state.content_length(recognition_list.lines().count());
+    
+            let recognition_list_paragraph = Paragraph::new(recognition_list)
+                .block(create_block("Recognized Tracks")).style(Style::default().fg(Color::Blue))
+                .wrap(Wrap { trim: true }).scroll((self.vertical_scroll as u16, 0));
+    
+            f.render_widget(recognition_list_paragraph, bottom_chunks[0]);
+            f.render_stateful_widget(
+                Scrollbar::new(ScrollbarOrientation::VerticalRight)
+                    .begin_symbol(Some("↑"))
+                    .end_symbol(Some("↓")),
+                bottom_chunks[0], &mut self.vertical_scroll_state);
+    
+            // Render recognition info
+            let mut recognition_info_text = String::new();
+            if let Some(timeout) = self.recognition_result_display_timeout {
+                if timeout.elapsed().unwrap() < Duration::from_secs(RECOGNITION_INFO_TIMER) {
+                    recognition_info_text = recognition_result_text.to_string();
+                } else {
+                    self.recognition_result_display_timeout = None;
+                    recognition_info_text = String::new();
+                }
+            }
+            let recognition_info_paragraph = Paragraph::new(recognition_info_text)
+                .block(create_block("Info")).style(Style::default().fg(Color::Blue))
+                .wrap(Wrap { trim: true });
+            f.render_widget(recognition_info_paragraph, bottom_chunks[1]);
+    
+            // Render controls
+            let controls = "j/k: Scroll Recognized Tracks | Enter: Play | s: Stop | </>: Volume | r: Recognise | =/-: Change duration | q: Quit".to_string();
+            let mut controls_text = controls.clone();
             let current_volume = self.volume;
             let volume_percentage = (current_volume * 100.0).round();
+            if let Some(timeout) = self.duration_display_timeout {
+                if timeout.elapsed().unwrap() < Duration::from_secs(DURATION_INFO_TIMER) {
+                    controls_text = format!("{}\nDuration: {}s", controls, self.duration);
+                } else {
+                    self.duration_display_timeout = None;
+                }
+            }
             if let Some(timeout) = self.volume_display_timeout {
-                 if timeout.elapsed().unwrap() < Duration::from_secs(2) {
-                     recognition_text = format!("{}\nVolume: {}%", recognition_text, volume_percentage);
-                 } else {
-                     self.volume_display_timeout = None;
-                 }
-             }
-            let duration_text = format!("{} seconds \nResult: {}", self.duration, recognition_text);
-            let recognition = Paragraph::new(duration_text)
-                .block(create_block("Track Recognition")).style(Style::default().fg(Color::Blue))
-                .wrap(Wrap { trim: true });
-            f.render_widget(recognition, bottom_chunks[0]);
-
-            let controls_text = "j/k: Move up/down | Enter: Play | s: Stop | </>: Volume | r: Recognise | +/-: Change duration | q: Quit".to_string();
-            let controls = Paragraph::new(controls_text).block(create_block("Controls")).style(Style::default().fg(Color::DarkGray)).wrap(Wrap { trim: true });
-            f.render_widget(controls, bottom_chunks[1]);
+                if timeout.elapsed().unwrap() < Duration::from_secs(VOLUME_INFO_TIMER) {
+                    controls_text = format!("{}\nVolume: {}%", controls, volume_percentage);
+                } else {
+                    self.volume_display_timeout = None;
+                }
+            }
+            let controls_paragraph = Paragraph::new(controls_text).block(create_block("Controls")).style(Style::default().fg(Color::DarkGray)).wrap(Wrap { trim: true });
+            f.render_widget(controls_paragraph, bottom_chunks[2]);
         })?;
         Ok(())
     }
@@ -456,11 +535,11 @@ impl Radio {
                 execute!(io::stdout(), LeaveAlternateScreen)?;
                 std::process::exit(0);
             }
-            KeyCode::Char('j') | KeyCode::Down => {
+            KeyCode::Down => {
                 self.selected_stream_index =
                     (self.selected_stream_index + 1) % (self.streams_collection.mixtapes.len() + 2)
             }
-            KeyCode::Char('k') | KeyCode::Up => {
+            KeyCode::Up => {
                 self.selected_stream_index =
                     (self.selected_stream_index + self.streams_collection.mixtapes.len() + 1)
                         % (self.streams_collection.mixtapes.len() + 2)
@@ -472,19 +551,25 @@ impl Radio {
                     self.play(StreamType::Mixtape);
                 }
                 self.start_recognition();
+                self.recognition_result_display_timeout = Some(SystemTime::now());
+                self.start_recognition_info_timer();
             }
             KeyCode::Char('s') => self.stop(),
             KeyCode::Char('r') => {
                 if self.current_stream_url.is_some() {
                     self.start_recognition();
+                    self.recognition_result_display_timeout = Some(SystemTime::now());
+                    self.start_recognition_info_timer();
                 }
             }
-            KeyCode::Char('+') => {
+            KeyCode::Char('=') => {
                 self.duration += 1;
+                self.duration_display_timeout = Some(SystemTime::now());
             }
             KeyCode::Char('-') => {
                 if self.duration > 1 {
                     self.duration -= 1;
+                    self.duration_display_timeout = Some(SystemTime::now());
                 }
             }
             KeyCode::Char('<') => {
@@ -504,6 +589,16 @@ impl Radio {
                         self.volume_display_timeout = Some(SystemTime::now());
                     }
                 }
+            }
+            KeyCode::Char('j') => {
+                self.vertical_scroll = self.vertical_scroll.saturating_add(1);
+                self.vertical_scroll_state =
+                    self.vertical_scroll_state.position(self.vertical_scroll);
+            }
+            KeyCode::Char('k') => {
+                self.vertical_scroll = self.vertical_scroll.saturating_sub(1);
+                self.vertical_scroll_state =
+                    self.vertical_scroll_state.position(self.vertical_scroll);
             }
             _ => {}
         }
